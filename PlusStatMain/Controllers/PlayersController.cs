1956fb00976af2597ecbf499566268b0a4c63170
Rasheek Huq <rhawk1509@gmail.com>
2025-01-02 15:21:32 -0600

    changed main project type to include controllers and swagger

---
+++ PlusStatMain/Controllers/PlayersController.cs
@@ -0,0 +1,22 @@
+﻿using DataTypes;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PlusStatMain.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PlayersController : Controller
+    {
+        [HttpGet]
+        public List<PlayerInfo> GetPlayers()
+        {
+            return new List<PlayerInfo>();
+        }
+
+        [HttpGet("{playerID}")]
+        public PlayerInfo GetPlayer(int playerID)
+        {
+            return new PlayerInfo();
+        }
+    }
+}
--- PlusStatMain/Program.cs
+++ PlusStatMain/Program.cs
@@ -1,14 +1,32 @@
-using Microsoft.Extensions.Configuration;
 using DataTypes;
 using DataAccess;
+using Microsoft.Extensions.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// declare database access class
+IConfiguration configuration = new ConfigurationBuilder().SetBasePath(Environment.CurrentDirectory).AddJsonFile("appSettings.json").Build();
+builder.Services.AddSingleton<I_DbAccess, SqlDbAccess>(provider => new SqlDbAccess(configuration.GetConnectionString("PlusStatConnectionString")));
+
 var app = builder.Build();
 
-// Database connection
-IConfiguration configuration = new ConfigurationBuilder().SetBasePath(Environment.CurrentDirectory).AddJsonFile("appSettings.json").Build();
-SqlDbAccess db = new SqlDbAccess(configuration.GetConnectionString("PlusStatConnectionString"));
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
-app.MapGet("/", () => "Hello World!");
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
